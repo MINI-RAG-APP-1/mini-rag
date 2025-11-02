3a782e33e61f489493d234055bdcb692bce13e4f
MoNafea01 <monafe301@gmail.com>
2025-11-02 21:30:47 +0200

    connection to db

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = get_settings()
+    
+    # Startup
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGO_URI)
+    app.mongodb = app.mongodb_client[settings.MONGODB_NAME]
+    print("✅ Connected to MongoDB")
+    
+    yield
+    
+    # Shutdown
+    app.mongodb_client.close()
+    print("🛑 MongoDB connection closed")
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
